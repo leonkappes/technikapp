08cea15526f60c4484331aca44881c99bcbad382
Leon Kappes <leon@kappes.space>
2022-03-26 00:12:32 +0100

    add imprint

---
+++ src/routes/impressum.svelte
@@ -0,0 +1,19 @@
+<script>
+	import BaseCard from '$lib/Cards/BaseCard.svelte';
+</script>
+
+<div class="flex flex-col p-3">
+	<div class="bg-sky-700 rounded-lg">
+		<div class="flex flex-col items-center justify-center m-4 text-white">
+			<h1 class="text-5xl">Impressum</h1>
+		</div>
+		<div class="m-3 text-white">
+			<h2>Angaben gemäß § 5 TMG:</h2>
+			<p>Leon Kappes</p>
+			<h3>Postanschrift:</h3>
+			<p>Thomas-Mann-Straße 27<br />41469 Neuss<br /></p>
+			<h3>Kontakt:</h3>
+			<p>Telefon: 015255104720<br />E-Mail: <a class="text-stone-200 hover:text-white" href="mailto:info@elsetech.cloud">info@elsetech.cloud</a></p>
+		</div>
+	</div>
+</div>
